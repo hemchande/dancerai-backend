9f425f4292a7902d2fcfd508b8d5bf374da57937
hemchande <hemchandeisha@gmail.com>
2025-06-01 15:45:23 -0400

    changes

--- server.js
+++ server.js
@@ -37,7 +37,5 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong!' });
 });
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-}); 
+// Export the Express API
+module.exports = app; 
